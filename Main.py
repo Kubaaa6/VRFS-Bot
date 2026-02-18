fe933b0335fa8354883a90a948ca25a6d01b07e2
Kubaaa6 <k.zabratowski22@gmail.com>
2026-02-18 20:43:23 +0000

    VRFS Bot - Use environment variables for security

---
+++ Main.py
@@ -0,0 +1,17 @@
+﻿import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
+
+bot.run(TOKEN)
